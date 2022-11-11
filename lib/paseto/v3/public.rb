7b85fc2aada4009b842ac751dbe1e434d1e191bc
Joe Truba <joe@bannable.net>
2022-11-11 21:09:42 +0000

    V3::Public specs, add some specs for this
    Notably, these specs helped identify an issue where a V3::Public it was
    possible to initialize the class with a key that was not on secp384r1. The
    class now protects against this by copying the input key into an empty
    EC container that has its group pre-initialized to secp384r1. This raises
    an error when copying the public key, or alternatively raises on a #check_key
    call when the resulting point is not on the group curve.

--- lib/paseto/v3/public.rb
+++ lib/paseto/v3/public.rb
@@ -5,34 +5,45 @@
 module Paseto
   module V3
     class Public < Paseto::Key
-      # Size of (r || s) in an ECDSA secp384 signature
+      # Size of (r || s) in an ECDSA secp384r1 signature
       SIGNATURE_BYTE_LEN = 96
 
-      # Size of r | s in an ECDSA secp384 signature
+      # Size of r | s in an ECDSA secp384r1 signature
       SIGNATURE_PART_LEN = T.let(SIGNATURE_BYTE_LEN / 2, Integer)
 
+      # The ECDSA secp384r1 key underlying the instance.
       sig { returns(OpenSSL::PKey::EC) }
       attr_reader :key
 
+      # Create a new Public instance with a brand new EC key.
       sig { returns(Public) }
       def self.generate
-        new(key: OpenSSL::PKey::EC.generate("secp384r1"))
+        new(key: OpenSSL::PKey::EC.generate("secp384r1").to_der)
       end
 
+      # `key` must be either a DER or PEM encoded secp384r1 key.
+      # Encrypted PEM inputs are not supported.
       sig { params(key: String).void }
       def initialize(key:)
-        @key = T.let(OpenSSL::PKey::EC.new(key), OpenSSL::PKey::EC)
-
-        begin
-          @key.check_key
-        rescue OpenSSL::PKey::ECError => e
-          raise Paseto::CryptoError, e.message
-        end
+        # Parse the DER to an EC, then initialize an empty EC that permits only
+        # secp384r1, and copy the key values from the input to the empty EC.
+        # This ensures that we never accept keys that are off-curve or for groups
+        # other than secp384r1.
+        maybe_unsafe = OpenSSL::PKey::EC.new(key)
+        @key = T.let(OpenSSL::PKey::EC.new(OpenSSL::PKey::EC::Group.new("secp384r1")), OpenSSL::PKey::EC)
+        @key.private_key = maybe_unsafe.private_key
+        @key.public_key = maybe_unsafe.public_key
+        @key.check_key
 
         super(version: "v3", purpose: "public")
+      rescue OpenSSL::PKey::ECError => e
+        raise Paseto::CryptoError, e.message
       end
 
       # rubocop:disable Metrics/AbcSize
+
+      # Sign `message` and optional non-empty `footer` and return a PASETO Token.
+      # The resulting token may be bound to particular uses by passing a non-empty `implicit_assertion`.
       sig { params(message: String, footer: String, implicit_assertion: String).returns(Token) }
       def sign(message:, footer: "", implicit_assertion: "")
         raise ArgumentError, "no private key available" unless key.private?
@@ -51,6 +62,7 @@ module Paseto
         Token.new(payload:, purpose:, version:, footer:)
       end
 
+      # Verify the signature of `token`, with an optional non-empty `implicit_assertion`.
       sig { params(token: Token, implicit_assertion: String).returns(String) }
       def verify(token:, implicit_assertion: "")
         # OPTIONAL: verify footer is expected, constant-time
@@ -71,13 +83,13 @@ module Paseto
 
         m
       end
+
       # rubocop:enable Metrics/AbcSize
 
       private
 
-      # OpenSSL returns and expects ECDSA signatures in a DER-encoded ECDSA_SIG struct,
-      # but we need to be able to transport only (r || s) in big-endian form.
-      # These methods allow us to convert (r ||s ) -> ECDSA_SIG and vice-versa.
+      # Convert a string consisting of `(r || s)`` to a DER-encoded `ECDSA_SIG` structure
+      # that can be used by OpenSSL.
       sig { params(signature: String).returns(String) }
       def rs_to_asn1(signature)
         r = signature[0, SIGNATURE_PART_LEN] || ""
@@ -91,6 +103,8 @@ module Paseto
         ).to_der
       end
 
+      # Convert a DER-encoded `ECDSA_SIG` structure to a binary string `(r || s)`
+      # which can be encoded into our tokens.
       sig { params(signature: String).returns(String) }
       def asn1_to_rs(signature)
         OpenSSL::ASN1.decode(signature).value.map do |v|
--- spec/paseto/v3/public_spec.rb
+++ spec/paseto/v3/public_spec.rb
@@ -1,23 +1,158 @@
 # frozen_string_literal: true
 
 RSpec.describe Paseto::V3::Public do
-  subject(:crypt) { described_class.generate }
+  subject(:crypt) { described_class.new(key: key_pem) }
 
-  describe ".generate" do
-    it "returns an instance with the correct group"
+  let(:key_pem) do
+    # secp384r1 private key
+    <<~PKEY
+      -----BEGIN EC PRIVATE KEY-----
+      MIGkAgEBBDCc5XO3mKvqE9kiiBFtlr9rP9t0bm3qM+l3NuMMA0rkcNa7iJ2A93BV
+      DsZJ/mf0lKqgBwYFK4EEACKhZANiAATzxuXLt0DqMKEBVgGH4bhstxUkTJwIe66o
+      08kVU1zCl4DI1zIxDKfwrFQc6fpiREWBNcHqrCieqkkERShKmNToujwIgBjSo2vg
+      BP+3M0fMaefsef6sT4yFK4gVV/IGLKg=
+      -----END EC PRIVATE KEY-----
+    PKEY
   end
 
-  describe ".new"
+  describe ".generate" do
+    subject(:crypt) { described_class.generate }
 
-  describe "#version"
+    it { is_expected.to be_a(described_class) }
 
-  describe "#purpose"
+    it "has the expected group" do
+      expect(crypt.key.group.curve_name).to eq("secp384r1")
+    end
+  end
 
-  describe "#header"
+  describe ".new" do
+    it { is_expected.to be_a described_class }
 
-  describe "#key"
+    it "raises an error when the key is empty" do
+      expect { described_class.new(key: "") }.to raise_error(Paseto::CryptoError, "invalid curve name")
+    end
 
-  describe "#sign"
+    it "raises an error when the key is the wrong type" do
+      expect { described_class.new(key: nil) }.to raise_error(TypeError)
+    end
 
-  describe "#verify"
+    context "when the key is an invalid point" do
+      let(:key_pem) do
+        <<~INVALID_KEY
+          -----BEGIN EC PRIVATE KEY-----
+          MIGkAgEBBDDA1Tm0m7YhkfeVpFuarAJYVlHp2tQj+1fOBiLa10t9E8TiQO/hVfxB
+          vGaVEQwOheWgBwYFK4EEACKhZANiAASyGqmryZGqdpsq5gEDIfNvgC3AwSJxiBCL
+          XKHBTFRp+tCezLDOK/6V8KK/vVGBJlGFW6/I7ahyXprxS7xs7hPA9iz5YiuqXlu+
+          lbrIpZOz7b73hyQQCkvbBO/Avg+hPAk=                                                                        
+          -----END EC PRIVATE KEY-----
+        INVALID_KEY
+      end
+
+      it "raises an error" do
+        expect { crypt }.to raise_error(Paseto::CryptoError, "EVP_PKEY_public_check: invalid private key")
+      end
+    end
+
+    context "when the key is for a different EC group" do
+      let(:key_pem) do
+        <<~PRIME256V1
+          -----BEGIN EC PRIVATE KEY-----
+          MHcCAQEEIM1jvFNkK2dQc/zMb/qkGQfCGuhDNyYQauo6Foyn7BD9oAoGCCqGSM49
+          AwEHoUQDQgAEc1hdwW24ZIra/e+FD7HQsBk0yir5g7bsoGtfy90X8/Se/E5IbkGD
+          KF80qTx0c/IdyxoDwfvfuscl+9KFbNSiNg==
+          -----END EC PRIVATE KEY-----
+        PRIME256V1
+      end
+
+      it "raises an error" do
+        expect { crypt }.to raise_error(Paseto::CryptoError, "EC_KEY_set_public_key: incompatible objects")
+      end
+    end
+  end
+
+  describe "#version" do
+    it { expect(crypt.version).to eq("v3") }
+  end
+
+  describe "#purpose" do
+    it { expect(crypt.purpose).to eq("public") }
+  end
+
+  describe "#header" do
+    it { expect(crypt.header).to eq("v3.public") }
+  end
+
+  describe "#key" do
+    it { expect(crypt.key).to be_a(OpenSSL::PKey::EC) }
+
+    it "is the same as the input key" do
+      expect(crypt.key.to_pem).to eq(key_pem)
+    end
+  end
+
+  describe "#sign" do
+    subject(:token) { crypt.sign(message: '{"foo":"bar"}', footer: "baz", implicit_assertion: "test") }
+
+    it { is_expected.to be_a(Paseto::Token) }
+
+    context "with only a public key" do
+      let(:key_pem) do
+        <<~PUBLIC_KEY
+          -----BEGIN PUBLIC KEY-----
+          MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEmZZ5XSxUkU31FZSuQ6zzAY4IaGXT6b6f
+          lqQMbw/me7x++1vEufDnSdLEjLCGNY16OWtexCsigBTd6sxblgEKfXUYKZ/L8snJ
+          7RFBJ9CqUU8ZYKRZb7v1gkkLfK2JZb2M
+          -----END PUBLIC KEY-----
+        PUBLIC_KEY
+      end
+
+      it "raises an error" do
+        expect { token }.to raise_error(ArgumentError, "no private key available")
+      end
+    end
+  end
+
+  describe "#verify" do
+    subject(:verify) { crypt.verify(token: token, implicit_assertion: "test") }
+
+    let(:token) do
+      Paseto::Token.parse(
+        "v3.public.eyJmb28iOiJiYXIifVwJTfAz6v87ouQ0ctc8Iy6Cehuu0gAHWmXuKUQhIHOlNCWVLMjhksCAGd" \
+        "j3a9QvHPwUxGD1O8DS0-RyBDpMsZc3NifE1RiiirauQT4scm4e2uuVpj7cd3VaO8_E961LVw.YmF6"
+      )
+    end
+
+    it "returns the plain text" do
+      expect(verify).to eq('{"foo":"bar"}')
+    end
+
+    context "with an invalid signature" do
+      let(:token) do
+        Paseto::Token.parse(
+          "v3.public.eyJmb28iOiJiYXIifVwJTfAz6v87ouQ0ctc8Iy6Cehuu0gAHWmXuKUQhIHOlNCWVLMjhksCAGd" \
+          "j3a9QvHPwUxGD1O8DS0-RyBDpMsZc3NifE1RiiirauQTAAAAAAAAAAVpj7cd3VaO8_E961LVw.YmF6"
+        )
+      end
+
+      it "raises an error" do
+        expect { verify }.to raise_error(Paseto::InvalidSignature)
+      end
+    end
+
+    context "when the message is smaller than the signature size" do
+      let(:token) { Paseto::Token.parse("v3.public.YXNkZg") }
+
+      it "raises an error" do
+        expect { verify }.to raise_error(Paseto::ParseError, "message too short")
+      end
+    end
+
+    context "with a mismatched token type" do
+      let(:token) { Paseto::Token.parse("v4.public.YXNkZtafaHUveQPUAMlk9AWOmx9c1TWXcuE2x8FkhxIGd9iVc-subaSDKVf8nm65HVnen0PUYilrNMbXGlsyv7eyaA4") }
+
+      it "raises an error" do
+        expect { verify }.to raise_error(Paseto::ParseError, "incorrect header for key type v3.public")
+      end
+    end
+  end
 end
