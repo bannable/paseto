1789498174f4028921d9242eec45ea597a05579f
Joe Truba <joe@bannable.net>
2022-11-16 14:23:19 +0000

    Add specs for refactored validation
    Instead of using a `verify` boolean, separate the verified and non-verified
    paths into `decode` and `decode!`.

--- lib/paseto/errors.rb
+++ lib/paseto/errors.rb
@@ -1,39 +1,45 @@
-  # typed: strict
-  # frozen_string_literal: true
+# typed: strict
+# frozen_string_literal: true
 
-  module Paseto
-    # Generic superclass for all Paseto errors
-    class Error < StandardError; end
+module Paseto
+  # Generic superclass for all Paseto errors
+  class Error < StandardError; end
 
-    # Deserialized data did not include mandatory fields.
-    class ParseError < Error; end
+  # Deserialized data did not include mandatory fields.
+  class ParseError < Error; end
 
-    # Superclass for claim validation errors
-    class ValidationError < Error; end
+  # Superclass for claim validation errors
+  class ValidationError < Error; end
 
-    # Token is expired
-    class ExpiredToken < ValidationError; end
+  # Token is expired
+  class ExpiredToken < ValidationError; end
 
-    # Token has a nbf before the current time
-    class InactiveToken < ValidationError; end
+  # Token has a nbf before the current time
+  class InactiveToken < ValidationError; end
 
-    # Disallowed issuer
-    class InvalidIssuer < ValidationError; end
+  # Disallowed issuer
+  class InvalidIssuer < ValidationError; end
 
-    # Incorrect audience
-    class InvalidAudience < ValidationError; end
+  # Incorrect audience
+  class InvalidAudience < ValidationError; end
 
-    # Token issued in the future
-    class ImmatureToken < ValidationError; end
+  # Token issued in the future
+  class ImmatureToken < ValidationError; end
 
-    # A cryptographic primitive has failed for any reason,
-    # such as attempting to initialize a stream cipher with
-    # an invalid nonce.
-    class CryptoError < Error; end
+  # Unacceptable sub
+  class InvalidSubject < ValidationError; end
 
-    # An authenticator was forged or otherwise corrupt
-    class InvalidAuthenticator < CryptoError; end
+  # Missing or unacceptable jti
+  class InvalidTokenIdentifier < ValidationError; end
 
-    # A signature was forged or otherwise corrupt
-    class InvalidSignature < CryptoError; end
-  end
+  # A cryptographic primitive has failed for any reason,
+  # such as attempting to initialize a stream cipher with
+  # an invalid nonce.
+  class CryptoError < Error; end
+
+  # An authenticator was forged or otherwise corrupt
+  class InvalidAuthenticator < CryptoError; end
+
+  # A signature was forged or otherwise corrupt
+  class InvalidSignature < CryptoError; end
+end
